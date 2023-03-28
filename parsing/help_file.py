3d35a48a2c5bed007c6ef59824d4ea6596c45cf1
CodderLord <verevkin0909@gmail.com>
2023-03-28 13:10:08 +0200

    add file for  help

---
+++ parsing/help_file.py
@@ -0,0 +1,13 @@
+import datetime as dt
+
+
+NOW_time = dt.datetime.now()
+
+first_name_dict = {}
+second_name_dict = {}
+first_same_name_dict = {}
+second_same_name_dict = {}
+
+
+def translate_to_datatime(year: int, month: int, day: int, hour: int = 0, minute: int = 0):
+	return dt.datetime(year=year, month=month, day=day, hour=hour, minute=minute)
