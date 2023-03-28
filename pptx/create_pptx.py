946a1db2eedac1cca3f1e6a06de271da9f8afc1a
CodderLord <verevkin0909@gmail.com>
2023-03-28 13:08:20 +0200

    create pptx file

---
+++ pptx/create_pptx.py
@@ -0,0 +1,10 @@
+from pptx import Presentation
+
+
+class PresentationFile:
+	def __init__(self):
+		self.presentations = Presentation
+		__SLD_LAYOUT_TITLE_AND_CONTENT = 6
+
+	#def create_slide(self):
+	#	self.presentations
