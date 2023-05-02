ecc4c50b81d92c342b3153a1fb0fd4197e6a1666
CodderLord <verevkin0909@gmail.com>
2023-05-02 12:36:48 +0200

    add method to create pentagon

--- diagrams/diagrams.py
+++ diagrams/diagrams.py
@@ -3,6 +3,8 @@ import time
 import matplotlib.pyplot as plt
 import matplotlib.ticker
 from parsing.help_file import load_config_json
+
+import numpy as np
 matplotlib.use('QtAgg')
 
 
@@ -57,10 +59,17 @@ class DiagramsCreate:
 		self.signal.emit(85)
 		time.sleep(0.3)
 		# ratio exodus
-		self.create_ratio_diagram(self.team_info_dict['first_exodus_percent'], self.team_info_dict['second_exodus_percent'], 'exodus_graphics', 'Исходный процент соотношения сил')
+		self.create_ratio_diagram(
+			self.team_info_dict['first_exodus_percent'], self.team_info_dict['second_exodus_percent'], 'exodus_graphics', 'Исходный процент соотношения сил')
 		time.sleep(0.3)
 		# exodus diagram
-		# func
+		self.create_pentagon_diagram(
+			first_values=self.team_info_dict['first_percents_for_pentagon'],
+			second_values=self.team_info_dict['second_percents_for_pentagon'],
+			name='pentagon_graphics',
+			name_1=self.team_info_dict["name_1"],
+			name_2=self.team_info_dict["name_2"])
+		time.sleep(0.3)
 
 	def create_ratio_diagram(self, percent_1, percent_2, name, title):
 		fig, ax = plt.subplots()
@@ -100,4 +109,26 @@ class DiagramsCreate:
 		ax.set_title(f'{title}', color='white')
 		ax.set_ylabel('Победы', color='white')
 		ax.set_xlabel('')
+		plt.savefig(f'{self.path}/{name}.png')
+
+	def create_pentagon_diagram(self, name, first_values: list, second_values: list, name_1, name_2):
+		n = 5
+		angles = np.linspace(0, 2 * np.pi, n + 1)[:-1]
+		fig = plt.figure()
+		ax = fig.add_subplot(111, polar=True)
+		ax.fill(angles, first_values, alpha=0.4, facecolor='red')  # first_values must have 5 values
+		ax.fill(angles, second_values, alpha=0.4, facecolor='#004DFF')  # second_values must have 5 values
+		ax.set_thetagrids(np.degrees(angles))
+		ax.set_rlabel_position(0)
+		ax.set_xticklabels(['Коеф.', 'Опыт', 'История', 'Форма', 'Третья сторона'], color='white')
+		fig.set_facecolor('#192B45')
+		ax.set_facecolor('#132034')
+		ax.set_yticklabels([])
+		ax.tick_params(pad=16)
+		red_patch = plt.Rectangle((0, 0), 1, 1, fc="red", alpha=0.5)
+		blue_patch = plt.Rectangle((0, 0), 1, 1, fc="#004DFF", alpha=0.5)
+		legend = plt.legend([red_patch, blue_patch], [f'{name_1}', f'{name_2}'], loc='upper left', bbox_to_anchor=(-0.3, 1.13))
+		legend.get_frame().set_facecolor('#132034')
+		legend.get_frame().set_edgecolor('#132034')
+		plt.setp(legend.get_texts(), color='white')
 		plt.savefig(f'{self.path}/{name}.png')
