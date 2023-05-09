8979e09c162f30aab307d0114ea24e5db5cf0605
CodderLord <verevkin0909@gmail.com>
2023-05-09 14:35:32 +0200

    add to functional

--- DB/history_matches.py
+++ DB/history_matches.py
@@ -2,6 +2,15 @@ import sqlite3
 
 
 class DataBase:
-    def __init__(self):
-        self.con = sqlite3.connect("history_mathes.db")  # for commit
-        self.cursor = self.con.cursor()   # for execute --> fetchone/fetchall
+	def __init__(self):
+		self.con = sqlite3.connect("DB/history_matches.db")  # for commit
+		self.cursor = self.con.cursor()  # for execute --> fetchone/fetchall
+	
+	def show_all_matches(self):
+		pass
+	
+	def commit_match(self, title_matches, bo_matches, program_result, time_analytic):
+		self.cursor.execute(f'''INSERT INTO history_matches (title_matches,bo_matches,program_result,time_analytic)
+		VALUES ("{str(title_matches)}", {int(bo_matches)}, {int(program_result)}, "{str(time_analytic)}")''')
+		self.con.commit()
+		self.con.close()
