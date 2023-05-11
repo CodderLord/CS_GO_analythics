92051757f408b5f1c9c62d00acd2c05292d0eac2
CodderLord <verevkin0909@gmail.com>
2023-05-12 00:06:21 +0200

    add check matches

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from qt.global_window import LinkInputWindow
 from PyQt6.QtWidgets import QApplication
 import sys
-
+from DB.history_matches import DataBase
 
 if __name__ == '__main__':
-    APP = QApplication(sys.argv)
-    window = LinkInputWindow()
-    APP.exec()
+	DataBase().check_match()
+	APP = QApplication(sys.argv)
+	window = LinkInputWindow()
+	APP.exec()
